21a0ce81913fd692ba49800d5e00dd43de2c7444
SvetlanaCh22 <111069682+SvetlanaCh22@users.noreply.github.com>
2023-07-23 00:37:41 +0300

    Add files via upload
    Program.cs - решение первой задачи ДЗ
    1.jpg - решение третьей задачи ДЗ

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+namespace HomeWorkArh1
+{
+    class Scene
+    {
+        public int Id;
+        public PoligonalModel Models;
+        public Flash Flashes;
+
+        public Type method1(Type);
+        public Type method2(Type, Type);
+    }
+
+    interface IModelChangeObserver
+    {
+        public ApplyUpdateModel();
+    }
+
+    interface IModelChanger
+    {
+        public NotifyChange(IModelChanger sender);
+    }
+
+    class ModelStore : IModelChanger
+    {
+        public PoligonalModel Models;
+        public Scene Scenes;
+        public Flash Flashes;
+        public Camera Cameras;
+        private IModelChangeObserver changeObservers;
+
+        public Scene GetScena(Int);
+        public NotifyChange(IModelChanger);
+    }
+
+    class PoligonalModel
+    {
+        public Poligon Poligons;
+        public Texture Textures;
+    }
+
+    class Texture
+    {
+
+    }
+
+    class Poligon
+    {
+        public Point3D Points;
+    }
+
+    class Flash
+    {
+        public Point3D Location;
+        public Angle3D Angle;
+        public Color Color;
+        public float Power;
+
+        public void Rotate(Angle3D);
+        public void Move(Point3D);
+    }
+
+    class Camera
+    {
+        public Point3D Location;
+        public Angle3D Angle;
+
+        public void Rotate(Angle3D);
+        public void Move(Point3D);
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
